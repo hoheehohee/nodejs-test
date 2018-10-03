19943d47b28e9bca8f2f0c378cc144ade35237d3
hoheehohee <boycat87@gmail.com>
2018-10-03 20:03:35 +0900

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:9
 COPY package.json /src/package.json  
+COPY lib/db.js /src/lib/db.js
 RUN  cd /src; npm install 
 COPY . /src
 EXPOSE 3002
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const uuid = require('uuid');
-const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const cors = require('cors')
+const path = require('path');
 const routes = require('./routes/router');
 const slackBots = require('./services/SlackBots');
 const app = express();
-const id = uuid.v4();
 const port = 3002;
 
 app.set('dbconnecter', path.join(__dirname, 'dbconnecter'));
-// app.use(express.static(path.join(__dirname, 'dbconnecter')));
 app.use(cors());
 app.use(methodOverride()); // 추가 
 app.use(bodyParser.urlencoded({ extended: true }));
